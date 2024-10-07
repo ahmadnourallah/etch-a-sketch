01713f6c4b523ead0e01ba62db8ec50e74012e37
Ahmad Nourallah <ahmadnurallah@gmail.com>
2024-10-07 12:15:24 -0400

    Implement coloring functionality

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const gridContainer = document.querySelector("#grid-container");
 const colorPicker = document.querySelector(".color-picker");
 
+function colorGridElement (element) {
+    element.style.backgroundColor = colorPicker.value;
+}
+
 function fillGrid(x, y) {
 
     for (let i = 1; i <= x * y; i++) {
         let gridElement = document.createElement("div");
         gridElement.style.width = `calc(100% / ${x})`;
+
+        gridElement.addEventListener("click", event => colorGridElement(event.target));
+
+        gridElement.addEventListener("mouseover", event => {
+            if (event.buttons === 1) { 
+                colorGridElement(event.target);
+            }
+        });
 
         gridContainer.append(gridElement);
     }

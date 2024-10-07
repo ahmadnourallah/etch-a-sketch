118e9f34af114ba0833fb7dbca64a7fa70b5ba7e
Ahmad Nourallah <ahmadnurallah@gmail.com>
2024-10-07 11:22:14 -0400

    Modify fillGrid to specify the size of grid elements

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const gridContainer = document.querySelector("#grid-container");
 
-function fillGrid(boxNum) {
-    
-    for (let i = 1; i <= boxNum; i++) {
+function fillGrid(x, y) {
+
+    for (let i = 1; i <= x * y; i++) {
         let gridElement = document.createElement("div");
-        
+        gridElement.style.width = `calc(100% / ${x})`;
+
         gridContainer.append(gridElement);
     }
 }

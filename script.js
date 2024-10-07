1625e242ed331bd150bbb8d454cada476e4f2955
Ahmad Nourallah <ahmadnurallah@gmail.com>
2024-10-07 11:03:44 -0400

    Create a function to fill the grid with a specific number of divs

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const gridContainer = document.querySelector("#grid-container");
+
+function fillGrid(boxNum) {
+    
+    for (let i = 1; i <= boxNum; i++) {
+        let gridElement = document.createElement("div");
+        
+        gridContainer.append(gridElement);
+    }
+}
